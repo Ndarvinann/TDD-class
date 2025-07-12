18db1abdb1ccf431bd06df585f8b91028d5f2d28
DarvinAnn <ndarvinann@gmail.com>
2025-07-12 07:24:28 +0300

    taxes cycle 3

--- taxes.js
+++ taxes.js
@@ -1,4 +1,11 @@
-function calculateTax(earnings){
-    return 0;
+// function calculateTax(earnings){
+//      if (earnings > 12000) return 4000;
+//     return 0;
+// }
+//refactoring 
+function calculateTax(earnings) {
+  if (earnings <= 12000) return 0;
+  if (earnings <= 36000) return (earnings - 12000) * 0.2;
+  return (36000 - 12000) * 0.2 + (earnings - 36000) * 0.4;
 }
 module.exports = calculateTax;
--- test/taxestest.js
+++ test/taxestest.js
@@ -1,8 +1,16 @@
-const{ expect} = require('chai');
-const calculateTax = require('../taxes');
+const { expect } = require('chai');
+const calculateTax = require('../taxes'); 
 
-describe('calculatorTax', ()=>{
-    it('returns 0 for earnings below $12,000' ()=>{
-        expect(calculateTax(10000)).timedOut.equal(0); //should fail
-    })
-})                    
+describe('calculateTax', () => {  
+  it('returns 0 for earnings below £12,000', () => {  
+    expect(calculateTax(10000)).to.equal(0);  
+  });
+
+  it('returns 20% tax for earnings between £12k-£36k', () => {
+    expect(calculateTax(20000)).to.equal(1600); 
+  });
+
+  it('returns 40% tax for earnings above £36k', () => {
+    expect(calculateTax(50000)).to.equal(13200);
+  });
+});
